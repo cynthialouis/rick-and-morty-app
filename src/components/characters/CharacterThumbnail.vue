a69ad317bf289ada7c95afb6ca60485afacccbdf
Cynthia Louis <cynthialouis.dev@gmail.com>
2022-04-13 11:03:05 +0200

    display first 20 characters thumbnails

---
+++ src/components/characters/CharacterThumbnail.vue
@@ -0,0 +1,37 @@
+<template>
+	<li
+		class="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200"
+	>
+		<div class="flex-1 flex flex-col p-8">
+			<img
+				class="w-32 h-32 flex-shrink-0 mx-auto rounded-full"
+				:src="character.image"
+				alt=""
+			/>
+			<h3 class="mt-6 text-gray-900 text-sm font-medium">
+				{{ character.name }}
+			</h3>
+			<dl class="mt-1 flex-grow flex flex-col justify-between">
+				<dd class="text-gray-500 text-sm">
+					{{ character.species }}
+				</dd>
+			</dl>
+		</div>
+	</li>
+</template>
+
+<script>
+export default {
+	name: 'CharacterThumbnail',
+	components: {},
+	props: {
+		character: {
+			type: Object,
+			required: true,
+		},
+	},
+	data: () => ({}),
+	computed: {},
+	methods: {},
+}
+</script>
---
+++ src/views/Characters.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="p-16 bg-gray-50">
+		<ul
+			class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 border rounded"
+		>
+			<character-thumbnail
+				v-for="character in characters"
+				:character="character"
+				:key="character.id"
+			/>
+		</ul>
+	</div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+import CharacterThumbnail from '../components/characters/CharacterThumbnail.vue'
+
+export default {
+	name: 'Characters',
+	components: { CharacterThumbnail },
+	data: () => ({
+		is_loading: false,
+		characters: null,
+	}),
+	computed: {
+		...mapGetters('Characters', ['getCharacters']),
+	},
+	methods: {
+		...mapActions('Characters', ['fetchCharacters']),
+	},
+	async mounted() {
+		// if (this.getCharacters.length === 0) {
+		// 	// set loading screen
+		// 	this.is_loading = true
+		await this.fetchCharacters()
+		this.characters = this.getCharacters
+		// this.is_loading = false
+		// }
+	},
+}
+</script>
