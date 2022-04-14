0d08b31f960762f806b317e4a634f02e748cb4c3
Cynthia Louis <cynthialouis.dev@gmail.com>
2022-04-14 14:42:35 +0200

    add new component StatusesFilter.vue

---
+++ src/components/list/StatusesFilter.vue
@@ -0,0 +1,59 @@
+<template>
+	<div class="flex text-gray-700">
+		<div class="flex flex-col">
+			<div>
+				<input
+					type="checkbox"
+					class="mr-2"
+					v-model="checked_status"
+					value="alive"
+					@click="checkOne"
+				/>
+				<span>Alive</span>
+			</div>
+			<div>
+				<input
+					type="checkbox"
+					class="mr-2"
+					v-model="checked_status"
+					value="dead"
+					@click="checkOne"
+				/>
+				<span>Dead</span>
+			</div>
+			<div>
+				<input
+					type="checkbox"
+					class="mr-2"
+					v-model="checked_status"
+					value="unknown"
+					@click="checkOne"
+				/>
+				<span>Unknown</span>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'StatusesFilter',
+	components: {},
+	data: () => ({
+		checked_status: [],
+	}),
+	methods: {
+		/**
+		 * Can only check one value out of the three
+		 */
+		checkOne() {
+			this.checked_status = []
+		},
+	},
+	watch: {
+		checked_status(value) {
+			this.$emit('status', value[0])
+		},
+	},
+}
+</script>
