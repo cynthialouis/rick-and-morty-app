a0332bf2dc9687594f0c6881d4e968ded570ecae
Cynthia Louis <cynthialouis.dev@gmail.com>
2022-04-14 11:02:33 +0200

    new component Badge.vue

---
+++ src/components/ui/Badge.vue
@@ -0,0 +1,34 @@
+<template>
+	<div
+		class="inline-flex items-center justify-center px-3 py-1 text-xs font-bold rounded-full border"
+		:class="badge_classes"
+		data-context="badge"
+	>
+		<slot />
+	</div>
+</template>
+
+<script>
+const BADGE_COLORS = {
+	yellow: { bg: 'bg-yellow-100', text: 'text-yellow-800' },
+	blue: { bg: 'bg-blue-100', text: 'text-blue-800' },
+	pink: { bg: 'bg-pink-100', text: 'text-pink-800' },
+}
+
+export default {
+	name: 'Badge',
+	props: {
+		color: {
+			type: String,
+			default: null,
+		},
+	},
+	computed: {
+		badge_classes() {
+			return this.color
+				? [BADGE_COLORS[this.color].bg, BADGE_COLORS[this.color].text]
+				: []
+		},
+	},
+}
+</script>
