062f621cb0dcdc82be428cedb19cc11fe65b25fd
Cynthia Louis <cynthialouis.dev@gmail.com>
2022-04-14 11:33:09 +0200

    redirect to details page

--- src/components/characters/Characters.vue
+++ src/components/characters/Characters.vue
@@ -10,28 +10,32 @@
 				class="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200"
 				data-context="thumbnail"
 			>
-				<div class="flex-1 flex flex-col p-8">
-					<img
-						class="w-32 h-32 flex-shrink-0 mx-auto rounded-full"
-						:src="character.image"
-						alt=""
-						data-context="image"
-					/>
-					<h3
-						class="mt-6 text-gray-900 text-sm font-medium"
-						data-context="name"
-					>
-						{{ character.name }}
-					</h3>
-					<div class="mt-1 flex-grow flex flex-col justify-between">
-						<span
-							class="text-gray-500 text-sm"
-							data-context="specie"
+				<router-link :to="`/character/${character.id}`">
+					<div class="flex-1 flex flex-col p-8">
+						<img
+							class="w-32 h-32 flex-shrink-0 mx-auto rounded-full"
+							:src="character.image"
+							alt=""
+							data-context="image"
+						/>
+						<h3
+							class="mt-6 text-gray-900 text-sm font-medium"
+							data-context="name"
 						>
-							{{ character.species }}
-						</span>
+							{{ character.name }}
+						</h3>
+						<div
+							class="mt-1 flex-grow flex flex-col justify-between"
+						>
+							<span
+								class="text-gray-500 text-sm"
+								data-context="specie"
+							>
+								{{ character.species }}
+							</span>
+						</div>
 					</div>
-				</div>
+				</router-link>
 			</li>
 		</ul>
 	</div>
