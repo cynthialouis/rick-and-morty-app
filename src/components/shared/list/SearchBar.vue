3bf64a2f288b207f3ab22e5345835055b48a5cda
Cynthia Louis <cynthialouis.dev@gmail.com>
2022-04-16 07:54:59 +0200

    update search

--- src/components/shared/list/SearchBar.vue
+++ src/components/shared/list/SearchBar.vue
@@ -6,34 +6,9 @@
 				class="h-14 w-96 pl-10 pr-20 rounded-lg focus:shadow focus:outline-none"
 				placeholder="Search..."
 				v-model="search_param"
-				@keydown.enter="search"
+				@keyup="search"
 				data-context="search-input"
 			/>
-			<div class="absolute top-2 right-2">
-				<button
-					type="submit"
-					class="h-10 w-10 text-white"
-					@click="search"
-					:disabled="!search_param"
-					data-context="search-btn"
-				>
-					<!-- HeroIcon svg -->
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="h-6 w-6 text-gray-400"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-						stroke-width="2"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-						/>
-					</svg>
-				</button>
-			</div>
 		</div>
 	</div>
 </template>
